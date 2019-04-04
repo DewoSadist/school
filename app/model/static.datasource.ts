a5845bf7ecf73f84c5f4c1d3a0fddcf5017fd68b
DewoSadist <orynbaidaulet@gmail.com>
2019-04-04 16:29:37 +0600

    feat: default products

--- app/model/static.datasource.ts
+++ app/model/static.datasource.ts
@@ -6,12 +6,11 @@ export class StaticDataSource {
     private data: Product[];
 
     constructor() {
-        this.data = new Array<Product>(
-            new Product(1, "Kayak", "Watersports", 275),
-            new Product(2, "Lifejacket", "Watersports", 48.95),
-            new Product(3, "Soccer Ball", "Soccer", 19.50),
-            new Product(4, "Corner Flags", "Soccer", 34.95),
-            new Product(5, "Thinking Cap", "Chess", 16));
+        this.data = [new Product(1, "HTML/HTML", "Web", 275),
+            new Product(2, "CSS/SASS", "Web", 48.95),
+            new Product(3, "Angularjs", "Web framework", 19.50),
+            new Product(4, "Angular", "Web framework", 34.95),
+            new Product(5, "Python", "China chert", 16)];
     }
 
     getData(): Product[] {
